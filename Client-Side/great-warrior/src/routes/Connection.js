6d193d0d63f1ff1018197b9976fdc683cf3c1064
Almog Bakhshi <AlmogBakhshi@gmail.com>
2019-11-09 13:39:28 +0200

    fix

--- Client-Side/great-warrior/src/routes/Connection.js
+++ Client-Side/great-warrior/src/routes/Connection.js
@@ -1,9 +1,9 @@
 /// Enter your ipv4
-export const IP = 'http://10.0.0.2';
+export const IP = 'http://10.0.0.6';
 
 /// Enter your Facebook application ID
-export const facebookID = '396431487598480';
+export const facebookID = '';
 
 /// Enter your google OAuth client ID
-export const googleAndroidClientId = '657239431019-24pt83gdcjchgj70usj7inepnp3kji49.apps.googleusercontent.com';
-export const googleIosClientId = '657239431019-rcdgj9ed25tekpeedfm7mg0p6inl41jr.apps.googleusercontent.com';
+export const googleAndroidClientId = '';
+export const googleIosClientId = '';
