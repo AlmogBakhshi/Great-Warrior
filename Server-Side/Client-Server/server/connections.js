9e61f99fdad0ea2f14db5f2feac23aeb3416c4ab
Almog Bakhshi <AlmogBakhshi@gmail.com>
2019-12-14 19:54:30 +0200

    sql config

---
+++ Server-Side/Client-Server/server/connections.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // mssql connection
+    config: {
+        user: , // enter your sql user
+        password: , // enter your sql password
+        server: , // enter the server ip
+        database: , // enter the database name
+        options: { encrypt: false }
+    }
+}
